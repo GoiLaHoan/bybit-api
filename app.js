15a85ef6130a291a4a13cc692db8463b4a6dedb0
ngochuy1012 <vungochuyhd2000@gmail.com>
2024-01-08 02:20:33 +0700

    doneee 2

--- app.js
+++ app.js
@@ -125,7 +125,7 @@ app.get('/trade', async (req, res) => {
     }
 });
 
-const PORT = 3000;
+const PORT = 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
