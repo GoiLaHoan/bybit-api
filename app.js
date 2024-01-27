7af987f0cbd2d38f639150b607e7acc9c7604597
Nguyễn Văn Hoàn <67900737+GoiLaHoan@users.noreply.github.com>
2024-01-27 11:13:35 +0700

    Update port

--- app.js
+++ app.js
@@ -352,7 +352,7 @@ app.get('/ruttien', async (req, res) => {
     }
 });
 
-const PORT = 3000;
+const PORT = 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
