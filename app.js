e37b48e23414db863880a58fa4e0e6902cfe5a19
Nguyễn Văn Hoàn <67900737+GoiLaHoan@users.noreply.github.com>
2024-01-30 18:22:20 +0700

    Update app.js xoa trade 2

--- app.js
+++ app.js
@@ -74,6 +74,7 @@ app.get('/trade', async (req, res) => {
                 console.error(error);
             });
 
+        console.log('equityUSDT', equityUSDT);
 
         // Lấy giá mua và giá bán gần nhất của đồng coin
         await client
@@ -230,275 +231,275 @@ app.get('/trade', async (req, res) => {
     }
 });
 
-app.get('/trade2', async (req, res) => {
-    const { coinName1, coinName2, API_KEY, API_SECRET } = req.query;
-    const symbol1 = `${coinName1}USDT`;
-    const symbol2 = `${coinName2}USDT`;
+// app.get('/trade2', async (req, res) => {
+//     const { coinName1, coinName2, API_KEY, API_SECRET } = req.query;
+//     const symbol1 = `${coinName1}USDT`;
+//     const symbol2 = `${coinName2}USDT`;
 
 
-    // Initialize RestClientV5 with provided credentials
-    const client = new RestClientV5({
-        key: API_KEY,
-        secret: API_SECRET,
-        testnet: false,
-    });
+//     // Initialize RestClientV5 with provided credentials
+//     const client = new RestClientV5({
+//         key: API_KEY,
+//         secret: API_SECRET,
+//         testnet: false,
+//     });
 
-    // Call your trade function here passing the parameters from the request body
-    try {
-        let priceBuy1 = '0.01'
-        let priceBuy2 = '0.01'
-        let priceSell1 = '9999'
-        let priceSell2 = '9999'
-        // let equityUSDT = null
-        let equitySell1 = null
-        let equitySell2 = null
-        // let openOrder = []
+//     // Call your trade function here passing the parameters from the request body
+//     try {
+//         let priceBuy1 = '0.01'
+//         let priceBuy2 = '0.01'
+//         let priceSell1 = '9999'
+//         let priceSell2 = '9999'
+//         // let equityUSDT = null
+//         let equitySell1 = null
+//         let equitySell2 = null
+//         // let openOrder = []
 
-        // Lấy số dư USDT ví UNIFIED
-        // await client
-        //     .getWalletBalance({
-        //         accountType: 'UNIFIED',
-        //         coin: 'USDT',
-        //     })
-        //     .then((response) => {
-        //         const equity = response.result.list[0].coin[0].availableToWithdraw; // số lượng usdt đang có trong ví UNIFIED
+//         // Lấy số dư USDT ví UNIFIED
+//         // await client
+//         //     .getWalletBalance({
+//         //         accountType: 'UNIFIED',
+//         //         coin: 'USDT',
+//         //     })
+//         //     .then((response) => {
+//         //         const equity = response.result.list[0].coin[0].availableToWithdraw; // số lượng usdt đang có trong ví UNIFIED
 
-        //         equityUSDT = String(convertFloat(equity))
-        //     })
-        //     .catch((error) => {
-        //         console.error(error);
-        //     });
-
-
-        // Lấy giá mua gần nhất của đồng coin 1
-        await client
-            .getOrderbook({
-                category: 'spot',
-                symbol: symbol1,
-            })
-            .then((response) => {
-                priceBuy1 = response.result.a[0][0]; // giá mua gần nhất
-            })
-            .catch((error) => {
-                console.error(error);
-            });
+//         //         equityUSDT = String(convertFloat(equity))
+//         //     })
+//         //     .catch((error) => {
+//         //         console.error(error);
+//         //     });
 
 
-        // Mua giá gần nhất đồng coin 1
-        await client
-            .submitOrder({
-                category: 'spot',
-                symbol: symbol1,
-                side: 'Buy',
-                orderType: 'Limit',
-                qty: convertFloat('5' / priceBuy1),
-                price: priceBuy1,
-            })
-            .then((response) => {
-                console.log(response);
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-
-        // Lấy giá mua gần nhất của đồng coin 2
-        await client
-            .getOrderbook({
-                category: 'spot',
-                symbol: symbol2,
-            })
-            .then((response) => {
-                priceBuy2 = response.result.a[0][0]; // giá mua gần nhất
-            })
-            .catch((error) => {
-                console.error(error);
-            });
+//         // Lấy giá mua gần nhất của đồng coin 1
+//         await client
+//             .getOrderbook({
+//                 category: 'spot',
+//                 symbol: symbol1,
+//             })
+//             .then((response) => {
+//                 priceBuy1 = response.result.a[0][0]; // giá mua gần nhất
+//             })
+//             .catch((error) => {
+//                 console.error(error);
+//             });
 
 
-        // Mua giá gần nhất đồng coin 2
-        await client
-            .submitOrder({
-                category: 'spot',
-                symbol: symbol2,
-                side: 'Buy',
-                orderType: 'Limit',
-                qty: convertFloat('5' / priceBuy2),
-                price: priceBuy2,
-            })
-            .then((response) => {
-                console.log(response);
-            })
-            .catch((error) => {
-                console.error(error);
-            });
+//         // Mua giá gần nhất đồng coin 1
+//         await client
+//             .submitOrder({
+//                 category: 'spot',
+//                 symbol: symbol1,
+//                 side: 'Buy',
+//                 orderType: 'Limit',
+//                 qty: convertFloat('5' / priceBuy1),
+//                 price: priceBuy1,
+//             })
+//             .then((response) => {
+//                 console.log(response);
+//             })
+//             .catch((error) => {
+//                 console.error(error);
+//             });
+
+//         // Lấy giá mua gần nhất của đồng coin 2
+//         await client
+//             .getOrderbook({
+//                 category: 'spot',
+//                 symbol: symbol2,
+//             })
+//             .then((response) => {
+//                 priceBuy2 = response.result.a[0][0]; // giá mua gần nhất
+//             })
+//             .catch((error) => {
+//                 console.error(error);
+//             });
 
 
-        // Check coin 1 đã có trong ví chưa
-        await client
-            .getWalletBalance({
-                accountType: 'UNIFIED',
-                coin: coinName1,
-            })
-            .then((response) => {
-                const equity = response.result.list[0].coin[0].availableToWithdraw; // số lượng coin đang có trong ví
-                equitySell1 = String(convertFloat(equity))
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-
-        // Lấy giá mua gần nhất của đồng coin 1
-        await client
-            .getOrderbook({
-                category: 'spot',
-                symbol: symbol1,
-            })
-            .then((response) => {
-                priceSell1 = response.result.b[0][0]; //giá bán gần nhất
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-
-        // Bán giá gần nhất đồng coin 1
-        await client
-            .submitOrder({
-                category: 'spot',
-                symbol: symbol1,
-                side: 'Sell',
-                orderType: 'Limit',
-                qty: equitySell1, // bán hết
-                price: priceSell1,
-            })
-            .then((response) => {
-                console.log(response);
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-
-        // Check coin 2 đã có trong ví chưa
-        await client
-            .getWalletBalance({
-                accountType: 'UNIFIED',
-                coin: coinName2,
-            })
-            .then((response) => {
-                const equity = response.result.list[0].coin[0].availableToWithdraw; // số lượng coin đang có trong ví
-                equitySell2 = String(convertFloat(equity))
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-
-        // Lấy giá mua gần nhất của đồng coin 2
-        await client
-            .getOrderbook({
-                category: 'spot',
-                symbol: symbol2,
-            })
-            .then((response) => {
-                priceSell2 = response.result.b[0][0]; //giá bán gần nhất
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-
-        // Bán giá gần nhất đồng coin 2
-        await client
-            .submitOrder({
-                category: 'spot',
-                symbol: symbol2,
-                side: 'Sell',
-                orderType: 'Limit',
-                qty: equitySell2, // bán hết
-                price: priceSell2,
-            })
-            .then((response) => {
-                console.log(response);
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-
-        // async function placeSellOrder() {
-        //     await client
-        //         .submitOrder({
-        //             category: 'spot',
-        //             symbol,
-        //             side: 'Sell',
-        //             orderType: 'Limit',
-        //             qty: equitySell,
-        //             price: priceSell,
-        //         })
-        //         .then((response) => {
-        //             console.log(response);
-        //         })
-        //         .catch((error) => {
-        //             console.error(error);
-        //         });
-        // }
-
-        // async function cancelAllOrders() {
-        //     await client
-        //         .cancelAllOrders({
-        //             category: 'spot',
-        //             settleCoin: 'USDT',
-        //         })
-        //         .then((response) => {
-        //             console.log(response);
-        //         })
-        //         .catch((error) => {
-        //             console.error(error);
-        //         });
-        // }
-
-        // async function checkAndPlaceOrder() {
-        //     await client
-        //         .getActiveOrders({
-        //             category: 'spot',
-        //             symbol: symbol,
-        //             openOnly: 0,
-        //             limit: 1,
-        //         })
-        //         .then((response) => {
-        //             openOrder = response?.result?.list;
-        //         })
-        //         .catch((error) => {
-        //             console.error(error);
-        //         });
-
-        //     if (openOrder.length !== 0) {
-        //         await cancelAllOrders();
-        //         await placeSellOrder();
-        //     }
-        // }
-
-        // let iterations = 0; // Initialize a counter variable
-        // const maxIterations = 3; // Set the maximum number of iterations
-
-        // // Kiểm tra mỗi 1 giây
-        // const intervalId = setInterval(async () => {
-        //     await checkAndPlaceOrder();
-
-        //     // Tăng biến đếm sau mỗi lần lặp
-        //     iterations++;
-
-        //     // Kiểm tra điều kiện dừng
-        //     if (openOrder.length === 0 || iterations >= maxIterations) {
-        //         clearInterval(intervalId); // Dừng vòng lặp nếu đạt điều kiện
-        //     }
-        // }, 500);
+//         // Mua giá gần nhất đồng coin 2
+//         await client
+//             .submitOrder({
+//                 category: 'spot',
+//                 symbol: symbol2,
+//                 side: 'Buy',
+//                 orderType: 'Limit',
+//                 qty: convertFloat('5' / priceBuy2),
+//                 price: priceBuy2,
+//             })
+//             .then((response) => {
+//                 console.log(response);
+//             })
+//             .catch((error) => {
+//                 console.error(error);
+//             });
 
 
-        // Return a success response
-        res.json({ message: 'Trade executed successfully' });
+//         // Check coin 1 đã có trong ví chưa
+//         await client
+//             .getWalletBalance({
+//                 accountType: 'UNIFIED',
+//                 coin: coinName1,
+//             })
+//             .then((response) => {
+//                 const equity = response.result.list[0].coin[0].availableToWithdraw; // số lượng coin đang có trong ví
+//                 equitySell1 = String(convertFloat(equity))
+//             })
+//             .catch((error) => {
+//                 console.error(error);
+//             });
 
-    } catch (error) {
-        // Return an error response
-        res.status(500).json({ error: error.message });
-    }
-});
+//         // Lấy giá mua gần nhất của đồng coin 1
+//         await client
+//             .getOrderbook({
+//                 category: 'spot',
+//                 symbol: symbol1,
+//             })
+//             .then((response) => {
+//                 priceSell1 = response.result.b[0][0]; //giá bán gần nhất
+//             })
+//             .catch((error) => {
+//                 console.error(error);
+//             });
+
+//         // Bán giá gần nhất đồng coin 1
+//         await client
+//             .submitOrder({
+//                 category: 'spot',
+//                 symbol: symbol1,
+//                 side: 'Sell',
+//                 orderType: 'Limit',
+//                 qty: equitySell1, // bán hết
+//                 price: priceSell1,
+//             })
+//             .then((response) => {
+//                 console.log(response);
+//             })
+//             .catch((error) => {
+//                 console.error(error);
+//             });
+
+//         // Check coin 2 đã có trong ví chưa
+//         await client
+//             .getWalletBalance({
+//                 accountType: 'UNIFIED',
+//                 coin: coinName2,
+//             })
+//             .then((response) => {
+//                 const equity = response.result.list[0].coin[0].availableToWithdraw; // số lượng coin đang có trong ví
+//                 equitySell2 = String(convertFloat(equity))
+//             })
+//             .catch((error) => {
+//                 console.error(error);
+//             });
+
+//         // Lấy giá mua gần nhất của đồng coin 2
+//         await client
+//             .getOrderbook({
+//                 category: 'spot',
+//                 symbol: symbol2,
+//             })
+//             .then((response) => {
+//                 priceSell2 = response.result.b[0][0]; //giá bán gần nhất
+//             })
+//             .catch((error) => {
+//                 console.error(error);
+//             });
+
+//         // Bán giá gần nhất đồng coin 2
+//         await client
+//             .submitOrder({
+//                 category: 'spot',
+//                 symbol: symbol2,
+//                 side: 'Sell',
+//                 orderType: 'Limit',
+//                 qty: equitySell2, // bán hết
+//                 price: priceSell2,
+//             })
+//             .then((response) => {
+//                 console.log(response);
+//             })
+//             .catch((error) => {
+//                 console.error(error);
+//             });
+
+//         // async function placeSellOrder() {
+//         //     await client
+//         //         .submitOrder({
+//         //             category: 'spot',
+//         //             symbol,
+//         //             side: 'Sell',
+//         //             orderType: 'Limit',
+//         //             qty: equitySell,
+//         //             price: priceSell,
+//         //         })
+//         //         .then((response) => {
+//         //             console.log(response);
+//         //         })
+//         //         .catch((error) => {
+//         //             console.error(error);
+//         //         });
+//         // }
+
+//         // async function cancelAllOrders() {
+//         //     await client
+//         //         .cancelAllOrders({
+//         //             category: 'spot',
+//         //             settleCoin: 'USDT',
+//         //         })
+//         //         .then((response) => {
+//         //             console.log(response);
+//         //         })
+//         //         .catch((error) => {
+//         //             console.error(error);
+//         //         });
+//         // }
+
+//         // async function checkAndPlaceOrder() {
+//         //     await client
+//         //         .getActiveOrders({
+//         //             category: 'spot',
+//         //             symbol: symbol,
+//         //             openOnly: 0,
+//         //             limit: 1,
+//         //         })
+//         //         .then((response) => {
+//         //             openOrder = response?.result?.list;
+//         //         })
+//         //         .catch((error) => {
+//         //             console.error(error);
+//         //         });
+
+//         //     if (openOrder.length !== 0) {
+//         //         await cancelAllOrders();
+//         //         await placeSellOrder();
+//         //     }
+//         // }
+
+//         // let iterations = 0; // Initialize a counter variable
+//         // const maxIterations = 3; // Set the maximum number of iterations
+
+//         // // Kiểm tra mỗi 1 giây
+//         // const intervalId = setInterval(async () => {
+//         //     await checkAndPlaceOrder();
+
+//         //     // Tăng biến đếm sau mỗi lần lặp
+//         //     iterations++;
+
+//         //     // Kiểm tra điều kiện dừng
+//         //     if (openOrder.length === 0 || iterations >= maxIterations) {
+//         //         clearInterval(intervalId); // Dừng vòng lặp nếu đạt điều kiện
+//         //     }
+//         // }, 500);
+
+
+//         // Return a success response
+//         res.json({ message: 'Trade executed successfully' });
+
+//     } catch (error) {
+//         // Return an error response
+//         res.status(500).json({ error: error.message });
+//     }
+// });
 app.get('/ruttien', async (req, res) => {
     const { API_KEY, API_SECRET, diachiruttien } = req.query;
 
