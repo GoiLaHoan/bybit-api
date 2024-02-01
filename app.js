34036a45c4ca897ae2e31a9feefb7cfcd205b74a
Nguyễn Văn Hoàn <67900737+GoiLaHoan@users.noreply.github.com>
2024-02-01 17:49:10 +0700

    Update app.js Huy2

--- app.js
+++ app.js
@@ -231,19 +231,28 @@ app.get('/trade', async (req, res) => {
 });
 
 const arrData = [
-    {
-        apiKey: 'whg9Lp6RWsaEllzBpZ',
-        secretKey: 'QDBtKNoe55MAxtL0LLOmdu5am1ESkcdpn5b6'
-    },
-    {
-        apiKey: 'SdCHr7s0r8mK8VQ6vV',
-        secretKey: 'H8v8mbU8Ep3vwKAiF5FgLZLy9aHBUtWwa0FA'
-    },
-    {
-        apiKey: 'MZID18SqCsvJTXFTD3',
-        secretKey: 'KLPVhsddmkEuvE6KmBZDAuUBNLUuw2WdOYD2'
-    },
-]
+    { apiKey: 'whg9Lp6RWsaEllzBpZ', secretKey: 'QDBtKNoe55MAxtL0LLOmdu5am1ESkcdpn5b6' },
+    { apiKey: 'SdCHr7s0r8mK8VQ6vV', secretKey: 'H8v8mbU8Ep3vwKAiF5FgLZLy9aHBUtWwa0FA' },
+    { apiKey: 'MZID18SqCsvJTXFTD3', secretKey: 'KLPVhsddmkEuvE6KmBZDAuUBNLUuw2WdOYD2' },
+    { apiKey: 'EyFkv4rDBdlYMiHbBV', secretKey: 'nrw5wcEVPc3dmUckYlcu8hBEf29Ah5DD5Nhs' },
+    { apiKey: 'uhFED0bfcALgRcFtgf', secretKey: 'hWPy2A4E7r5RBWGwLovSdVShMFtHNxi0raZ8' },
+    { apiKey: 'seTMBvPyFZdFAHSNts', secretKey: 'QZ89wRAw489AsvziIvbS2DabeIiF2qhs45fN' },
+    { apiKey: 'xwsJ4HF7pbedcS4Q6R', secretKey: 'HlBcL1xyNdYxhFhGLJ6UXluvkpij1M4zcTFp' },
+    { apiKey: 'pWDtoKBqmXPnYh4HRe', secretKey: 'b0wKmcc94a9fj9gwcnYMAL4leCgVHR8hkwiO' },
+    { apiKey: '6hRjX1wghSoXsH9fKn', secretKey: '51g4pwjeD6YT8Ed6RCCmMg89E3epwGkr429b' },
+    { apiKey: 'vzTqX5MhNxjQXPHWCK', secretKey: 'zvNjQ4cIMohrnRo2czqjcKpQYMvFrwnPsIin' },
+    { apiKey: '6UkT2PaKJtbsba2BiB', secretKey: 'g5Eenaw3br41sFZ6t4tNTeht02slXqZpt3eg' },
+    { apiKey: 'bCtzcQwDwxK5XDSvWN', secretKey: '5NwvV6aU3wgglQ9uJWp8C473M7ncGqyHkDce' },
+    { apiKey: 'ASDXOp4TT8p7HwD5Kk', secretKey: 'zpguOGNC8sE23oKZHh2YMvSSuA84mEA8uWdb' },
+    { apiKey: 'b8PDTwkByQvuULtbCz', secretKey: '5itzUOergKoQRbGWAWeF9XrxEwPtZpVlF3qK' },
+    { apiKey: 'A5mCHigKIVfQQ6IcTh', secretKey: 'ue4kJP9INHh3O9oOSKzOCZG4FWjMKIktbCfy' },
+    { apiKey: 'zBsXqax2KgyQLFssQF', secretKey: 'zyWqRdUveUk6tvcbrNIiCS39V3Ojkh69SsOp' },
+    { apiKey: 'uesUi3Eu9nhhK6TkJE', secretKey: 'elaQO0BeN2DXkdLzaAvg1MtF6Pz6PipAdAbk' },
+    { apiKey: 'YOSnzlV8vmNk1lTasr', secretKey: 's8F0aaQOrLcAPnd5c0zS67yqVx3O060I27as' },
+    { apiKey: '5S4lmd2Bv0MbBPAAGK', secretKey: '4hk8PmG7JcYk98uEhLFx4EWEBBkH8xtbeniC' },
+    { apiKey: 'XH4JpFK5M6Ai7tuEGB', secretKey: 'Ll6O8KnTdibnbMKILb0OXPPSs1mHLbGaoB81' }
+  ];
+  
 
 // Define the POST endpoint
 app.get('/tradeHuy', async (req, res) => {
@@ -429,6 +438,8 @@ app.get('/tradeHuy', async (req, res) => {
     arrData.forEach(element => {
         trade(coinName, element.apiKey, element.secretKey)
     });
+
+    res.json({ message: 'Trade executed successfully' });
     
 });
 
