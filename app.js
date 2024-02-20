2aefd739b337ffe1aa4dde6ecbc1158dd2eb329b
ngochuy1012 <vungochuyhd2000@gmail.com>
2024-02-20 15:55:31 +0700

    update trade loop

--- app.js
+++ app.js
@@ -1059,6 +1059,207 @@ app.get('/ruttien', async (req, res) => {
     }
 });
 
+app.get('/tradeLoop', async (req, res) => {
+    const { coinName, API_KEY, API_SECRET } = req.query;
+    let totalTrade = 0;
+    const client = new RestClientV5({
+        key: API_KEY,
+        secret: API_SECRET,
+        testnet: false,
+    })
+
+    const symbol = `${coinName}USDT`;
+    const trade = async () => {
+
+
+        let priceBuy = '0.01'
+        let priceSell = '9999'
+        let equityUSDT = null
+        let equitySell = null
+        let openOrder = []
+
+        // Lấy số dư USDT ví UNIFIED
+        await client
+            .getWalletBalance({
+                accountType: 'UNIFIED',
+                coin: 'USDT',
+            })
+            .then((response) => {
+                const equity = response.result.list[0].coin[0].equity; // số lượng usdt đang có trong ví UNIFIED
+
+                equityUSDT = String(convertFloat(equity))
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+
+
+        // Lấy giá mua và giá bán gần nhất của đồng coin
+        await client
+            .getOrderbook({
+                category: 'spot',
+                symbol,
+            })
+            .then((response) => {
+                priceBuy = response.result.a[0][0]; // giá mua gần nhất
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+
+
+        // Mua giá gần nhất
+        await client
+            .submitOrder({
+                category: 'spot',
+                symbol,
+                side: 'Buy',
+                orderType: 'Limit',
+                qty: convertFloat(equityUSDT / priceBuy),
+                price: priceBuy,
+            })
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+
+
+        // Check coin đã có trong ví chưa
+        await client
+            .getWalletBalance({
+                accountType: 'UNIFIED',
+                coin: coinName,
+            })
+            .then((response) => {
+                const equity = response.result.list[0].coin[0].equity; // số lượng coin đang có trong ví
+                equitySell = String(convertFloat(equity))
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+
+        // Lấy giá mua và giá bán gần nhất của đồng coin
+        await client
+            .getOrderbook({
+                category: 'spot',
+                symbol,
+            })
+            .then((response) => {
+                priceSell = response.result.b[0][0]; //giá bán gần nhất
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+
+        // Bán giá gần nhất
+        await client
+            .submitOrder({
+                category: 'spot',
+                symbol,
+                side: 'Sell',
+                orderType: 'Limit',
+                qty: equitySell, // bán hết
+                price: priceSell,
+            })
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+
+        async function placeSellOrder() {
+            await client
+                .submitOrder({
+                    category: 'spot',
+                    symbol,
+                    side: 'Sell',
+                    orderType: 'Limit',
+                    qty: equitySell,
+                    price: priceSell,
+                })
+                .then((response) => {
+                    console.log(response);
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        }
+
+        async function cancelAllOrders() {
+            await client
+                .cancelAllOrders({
+                    category: 'spot',
+                    settleCoin: 'USDT',
+                })
+                .then((response) => {
+                    console.log(response);
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        }
+
+        async function checkAndPlaceOrder() {
+            await client
+                .getActiveOrders({
+                    category: 'spot',
+                    symbol: symbol,
+                    openOnly: 0,
+                    limit: 1,
+                })
+                .then((response) => {
+                    openOrder = response?.result?.list;
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+
+            if (openOrder.length !== 0) {
+                await cancelAllOrders();
+                await placeSellOrder();
+            }
+        }
+
+        let iterations = 0; // Initialize a counter variable
+        const maxIterations = 3; // Set the maximum number of iterations
+
+        // Kiểm tra mỗi 1 giây
+        const intervalId = setInterval(async () => {
+            await checkAndPlaceOrder();
+
+            // Tăng biến đếm sau mỗi lần lặp
+            iterations++;
+
+            // Kiểm tra điều kiện dừng
+            if (openOrder.length === 0 || iterations >= maxIterations) {
+                clearInterval(intervalId); // Dừng vòng lặp nếu đạt điều kiện
+            }
+        }, 500);
+
+    }
+
+    while (totalTrade <= 105) {
+        await trade()
+        await client
+            .getExecutionList({
+                category: 'spot',
+                symbol: symbol,
+            })
+            .then((response) => {
+                totalTrade = response.result.list.reduce((acc, curr) => acc + parseFloat(curr.execValue), 0)
+                console.log(totalTrade);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+    res.json({ message: 'Trade executed successfully' });
+
+});
+
 const PORT = 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
