001ab446d6273e955850f4557316de26a2bab22c
ngochuy1012 <vungochuyhd2000@gmail.com>
2024-03-08 10:11:59 +0700

    add port env

--- app.js
+++ app.js
@@ -909,7 +909,7 @@ app.get('/checkCoin', async (req, res) => {
 
 });
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
