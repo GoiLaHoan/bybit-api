b7f113397599ced9da319a8936c44a0977ae7a58
ngochuy1012 <vungochuyhd2000@gmail.com>
2024-03-14 16:17:06 +0700

    update dataHuy

--- dataHuy.js
+++ dataHuy.js
@@ -190,197 +190,197 @@
 //     }
 // ];
 
-const dataHuy1 = [
-    {
-        apiKey: 'Cxcew2Xdx7rwqX0f3b',
-        secretKey: 'BXPgPxOIMgF8rYDzyzfDLJawUdhB9MBKAlh3',
-        proxy: {
-            host: "qna03.vitechcheap.com",
-            port: 28074,
-            auth: { username: "user_gnkogl", password: "hbwy0u7t" },
-        }
-    },
-    {
-        apiKey: 'W32HHanwPIld4CLgBs',
-        secretKey: 'Mg5HKMCtjjP89jz3xrFoKTKdCSjzp3oJVuA7',
-        proxy: {
-            host: "qna03.vitechcheap.com",
-            port: 28068,
-            auth: { username: "user_gnkogl", password: "hbwy0u7t" },
-        }
-    },
-    {
-        apiKey: 'fGwy5PZMWy6lcAqfra',
-        secretKey: 'FuEx7NuI8GozpXFIBxbH4ROxDJW9fv43Cvej',
-        proxy: {
-            host: "qna03.vitechcheap.com",
-            port: 28063,
-            auth: { username: "user_gnkogl", password: "hbwy0u7t" },
-        }
-    },
-    {
-        apiKey: 'myD9ePm96ITbMTZQ8h',
-        secretKey: 'Js6sxuchyhib7ZpsInUZfy6JhSUAUk8tvOI4',
-        proxy: {
-            host: "qna03.vitechcheap.com",
-            port: 28069,
-            auth: { username: "user_gnkogl", password: "hbwy0u7t" },
-        }
-    },
-]
+// const dataHuy1 = [
+//     {
+//         apiKey: 'Cxcew2Xdx7rwqX0f3b',
+//         secretKey: 'BXPgPxOIMgF8rYDzyzfDLJawUdhB9MBKAlh3',
+//         proxy: {
+//             host: "qna03.vitechcheap.com",
+//             port: 28074,
+//             auth: { username: "user_gnkogl", password: "hbwy0u7t" },
+//         }
+//     },
+//     {
+//         apiKey: 'W32HHanwPIld4CLgBs',
+//         secretKey: 'Mg5HKMCtjjP89jz3xrFoKTKdCSjzp3oJVuA7',
+//         proxy: {
+//             host: "qna03.vitechcheap.com",
+//             port: 28068,
+//             auth: { username: "user_gnkogl", password: "hbwy0u7t" },
+//         }
+//     },
+//     {
+//         apiKey: 'fGwy5PZMWy6lcAqfra',
+//         secretKey: 'FuEx7NuI8GozpXFIBxbH4ROxDJW9fv43Cvej',
+//         proxy: {
+//             host: "qna03.vitechcheap.com",
+//             port: 28063,
+//             auth: { username: "user_gnkogl", password: "hbwy0u7t" },
+//         }
+//     },
+//     {
+//         apiKey: 'myD9ePm96ITbMTZQ8h',
+//         secretKey: 'Js6sxuchyhib7ZpsInUZfy6JhSUAUk8tvOI4',
+//         proxy: {
+//             host: "qna03.vitechcheap.com",
+//             port: 28069,
+//             auth: { username: "user_gnkogl", password: "hbwy0u7t" },
+//         }
+//     },
+// ]
 
-const dataHuy2 = [
-    {
-        apiKey: '51ctcDxAxNx4sp6frU',
-        secretKey: 'Pd7aVPDILa7WHJA3Wp9mq6znallIHlFRI4eU',
-        proxy: {
-            host: "qna02.vitechcheap.com",
-            port: 27043,
-            auth: { username: "user_lvidz", password: "0a1orb6e" },
-        }
-    },
-    {
-        apiKey: 'HCSPRjfGZ4sidHXWMO',
-        secretKey: 'WdLBeeh5jypbHmP1vZFhippKJCRpZHSew8KA',
-        proxy: {
-            host: "qna02.vitechcheap.com",
-            port: 27024,
-            auth: { username: "user_lvidz", password: "0a1orb6e" },
-        }
-    },
-    {
-        apiKey: 'ETcf3IvuxvMlTPcGhv',
-        secretKey: 'goQGS1X2M4g0Z06ORgXhaod1WiqjvrVSOI9H',
-        proxy: {
-            host: "qna02.vitechcheap.com",
-            port: 27032,
-            auth: { username: "user_lvidz", password: "0a1orb6e" },
-        }
-    },
-    {
-        apiKey: 'Tft3kEKBZ1POFs3LrQ',
-        secretKey: 'MyMVr2osrwGsCOmBRiw1AeqGMlXeu0yv7frn',
-        proxy: {
-            host: "qna02.vitechcheap.com",
-            port: 27034,
-            auth: { username: "user_lvidz", password: "0a1orb6e" },
-        }
-    },
-]
-const dataHuy3 = [
-    {
-        apiKey: '4cdwoLpPvcqZREjgwC',
-        secretKey: 'GpOKPgZ8xAs6M1JkjmdPC3MMusJTmaimrQcg',
-        proxy: {
-            host: "qna02.vitechcheap.com",
-            port: 27035,
-            auth: { username: "user_lvidz", password: "0a1orb6e" },
-        }
-    },
-    {
-        apiKey: 'M8jW51YdEIu25WUfC4',
-        secretKey: 'YtLHcKZxxM3vZWK6H9MaEiQWdAQ8JlnDMn4P',
-        proxy: {
-            host: "qna02.vitechcheap.com",
-            port: 27039,
-            auth: { username: "user_lvidz", password: "0a1orb6e" },
-        }
-    },
-    {
-        apiKey: 'oXdBcSLBLbSJWZ4o2T',
-        secretKey: 'btakQB4KnZL8hPek73L1wD99ohskUVumCbRW',
-        proxy: {
-            host: "qna02.vitechcheap.com",
-            port: 27041,
-            auth: { username: "user_lvidz", password: "0a1orb6e" },
-        }
-    },
-    {
-        apiKey: 'xvI7ZIAFbMZiZjjgG7',
-        secretKey: 'yApuT9dOnx7e2bj1XusoWrkHtJ2e7r2sGPLU',
-        proxy: {
-            host: "qna02.vitechcheap.com",
-            port: 27045,
-            auth: { username: "user_lvidz", password: "0a1orb6e" },
-        }
-    },
-]
-const dataHuy4 = [
-    {
-        apiKey: 'CZ40IJtKepv5waPelu',
-        secretKey: '58zIHVMsubXPrZZxJDgw0dCASRwd9ZxuHyTQ',
-        proxy: {
-            host: "qna02.vitechcheap.com",
-            port: 27056,
-            auth: { username: "user_lvidz", password: "0a1orb6e" },
-        }
-    },
-    {
-        apiKey: 'ub2KFS2CEgqQkzwAXv',
-        secretKey: '16TkpUInDqXiRqNlPF1sodUhiXuIqHd6NkQ4',
-        proxy: {
-            host: "qna02.vitechcheap.com",
-            port: 27060,
-            auth: { username: "user_lvidz", password: "0a1orb6e" },
-        }
-    },
-    {
-        apiKey: 'GfmOZtY8tTCf36nmC6',
-        secretKey: 'pygenAHtTQcK0Elgu04nEB75oMfvsRPMJy1H',
-        proxy: {
-            host: "qna02.vitechcheap.com",
-            port: 27071,
-            auth: { username: "user_lvidz", password: "0a1orb6e" },
-        }
-    },
-    {
-        apiKey: 'KBRRZ4i4e6mXkvQBze',
-        secretKey: 'trXOtzGDZZ5JCf7kZN08GZxDxujhBLUa7MDk',
-        proxy: {
-            host: "qna02.vitechcheap.com",
-            port: 27051,
-            auth: { username: "user_lvidz", password: "0a1orb6e" },
-        }
-    },
-]
-const dataHuy5 = [
-    {
-        apiKey: 'wocJSGg2BDigsShlhA',
-        secretKey: 'jYzH4kiwu9at2IKaTeLOVuEphSID3DNG8U8R',
-        proxy: {
-            host: "qna02.vitechcheap.com",
-            port: 27048,
-            auth: { username: "user_lvidz", password: "0a1orb6e" },
-        }
-    },
-    {
-        apiKey: 'RADbCjkxxhqvMtK4Ud',
-        secretKey: '9f1VSmbByTATDXFoAbR4lPRSvBuTYhMnzDqU',
-        proxy: {
-            host: "qna02.vitechcheap.com",
-            port: 27062,
-            auth: { username: "user_lvidz", password: "0a1orb6e" },
-        }
-    },
-    {
-        apiKey: 'DjWUEJii55b3rDF9EV',
-        secretKey: 'TEgYJLtmfkPVh91HmK3QmYpEaguuOHYlrnvD',
-        proxy: {
-            host: "qna02.vitechcheap.com",
-            port: 27070,
-            auth: { username: "user_lvidz", password: "0a1orb6e" },
-        }
-    },
-    {
-        apiKey: 'mPZPIJAOBtsuOYSv04',
-        secretKey: 'jrpVnTcK2I8xHqyCW8qCkTTHvXdw9THk6vGW',
-        proxy: {
-            host: "qna02.vitechcheap.com",
-            port: 28052,
-            auth: { username: "user_gnkogl", password: "hbwy0u7t" },
-        }
-    }
-]
+// const dataHuy2 = [
+//     {
+//         apiKey: '51ctcDxAxNx4sp6frU',
+//         secretKey: 'Pd7aVPDILa7WHJA3Wp9mq6znallIHlFRI4eU',
+//         proxy: {
+//             host: "qna02.vitechcheap.com",
+//             port: 27043,
+//             auth: { username: "user_lvidz", password: "0a1orb6e" },
+//         }
+//     },
+//     {
+//         apiKey: 'HCSPRjfGZ4sidHXWMO',
+//         secretKey: 'WdLBeeh5jypbHmP1vZFhippKJCRpZHSew8KA',
+//         proxy: {
+//             host: "qna02.vitechcheap.com",
+//             port: 27024,
+//             auth: { username: "user_lvidz", password: "0a1orb6e" },
+//         }
+//     },
+//     {
+//         apiKey: 'ETcf3IvuxvMlTPcGhv',
+//         secretKey: 'goQGS1X2M4g0Z06ORgXhaod1WiqjvrVSOI9H',
+//         proxy: {
+//             host: "qna02.vitechcheap.com",
+//             port: 27032,
+//             auth: { username: "user_lvidz", password: "0a1orb6e" },
+//         }
+//     },
+//     {
+//         apiKey: 'Tft3kEKBZ1POFs3LrQ',
+//         secretKey: 'MyMVr2osrwGsCOmBRiw1AeqGMlXeu0yv7frn',
+//         proxy: {
+//             host: "qna02.vitechcheap.com",
+//             port: 27034,
+//             auth: { username: "user_lvidz", password: "0a1orb6e" },
+//         }
+//     },
+// ]
+// const dataHuy3 = [
+//     {
+//         apiKey: '4cdwoLpPvcqZREjgwC',
+//         secretKey: 'GpOKPgZ8xAs6M1JkjmdPC3MMusJTmaimrQcg',
+//         proxy: {
+//             host: "qna02.vitechcheap.com",
+//             port: 27035,
+//             auth: { username: "user_lvidz", password: "0a1orb6e" },
+//         }
+//     },
+//     {
+//         apiKey: 'M8jW51YdEIu25WUfC4',
+//         secretKey: 'YtLHcKZxxM3vZWK6H9MaEiQWdAQ8JlnDMn4P',
+//         proxy: {
+//             host: "qna02.vitechcheap.com",
+//             port: 27039,
+//             auth: { username: "user_lvidz", password: "0a1orb6e" },
+//         }
+//     },
+//     {
+//         apiKey: 'oXdBcSLBLbSJWZ4o2T',
+//         secretKey: 'btakQB4KnZL8hPek73L1wD99ohskUVumCbRW',
+//         proxy: {
+//             host: "qna02.vitechcheap.com",
+//             port: 27041,
+//             auth: { username: "user_lvidz", password: "0a1orb6e" },
+//         }
+//     },
+//     {
+//         apiKey: 'xvI7ZIAFbMZiZjjgG7',
+//         secretKey: 'yApuT9dOnx7e2bj1XusoWrkHtJ2e7r2sGPLU',
+//         proxy: {
+//             host: "qna02.vitechcheap.com",
+//             port: 27045,
+//             auth: { username: "user_lvidz", password: "0a1orb6e" },
+//         }
+//     },
+// ]
+// const dataHuy4 = [
+//     {
+//         apiKey: 'CZ40IJtKepv5waPelu',
+//         secretKey: '58zIHVMsubXPrZZxJDgw0dCASRwd9ZxuHyTQ',
+//         proxy: {
+//             host: "qna02.vitechcheap.com",
+//             port: 27056,
+//             auth: { username: "user_lvidz", password: "0a1orb6e" },
+//         }
+//     },
+//     {
+//         apiKey: 'ub2KFS2CEgqQkzwAXv',
+//         secretKey: '16TkpUInDqXiRqNlPF1sodUhiXuIqHd6NkQ4',
+//         proxy: {
+//             host: "qna02.vitechcheap.com",
+//             port: 27060,
+//             auth: { username: "user_lvidz", password: "0a1orb6e" },
+//         }
+//     },
+//     {
+//         apiKey: 'GfmOZtY8tTCf36nmC6',
+//         secretKey: 'pygenAHtTQcK0Elgu04nEB75oMfvsRPMJy1H',
+//         proxy: {
+//             host: "qna02.vitechcheap.com",
+//             port: 27071,
+//             auth: { username: "user_lvidz", password: "0a1orb6e" },
+//         }
+//     },
+//     {
+//         apiKey: 'KBRRZ4i4e6mXkvQBze',
+//         secretKey: 'trXOtzGDZZ5JCf7kZN08GZxDxujhBLUa7MDk',
+//         proxy: {
+//             host: "qna02.vitechcheap.com",
+//             port: 27051,
+//             auth: { username: "user_lvidz", password: "0a1orb6e" },
+//         }
+//     },
+// ]
+// const dataHuy5 = [
+//     {
+//         apiKey: 'wocJSGg2BDigsShlhA',
+//         secretKey: 'jYzH4kiwu9at2IKaTeLOVuEphSID3DNG8U8R',
+//         proxy: {
+//             host: "qna02.vitechcheap.com",
+//             port: 27048,
+//             auth: { username: "user_lvidz", password: "0a1orb6e" },
+//         }
+//     },
+//     {
+//         apiKey: 'RADbCjkxxhqvMtK4Ud',
+//         secretKey: '9f1VSmbByTATDXFoAbR4lPRSvBuTYhMnzDqU',
+//         proxy: {
+//             host: "qna02.vitechcheap.com",
+//             port: 27062,
+//             auth: { username: "user_lvidz", password: "0a1orb6e" },
+//         }
+//     },
+//     {
+//         apiKey: 'DjWUEJii55b3rDF9EV',
+//         secretKey: 'TEgYJLtmfkPVh91HmK3QmYpEaguuOHYlrnvD',
+//         proxy: {
+//             host: "qna02.vitechcheap.com",
+//             port: 27070,
+//             auth: { username: "user_lvidz", password: "0a1orb6e" },
+//         }
+//     },
+//     {
+//         apiKey: 'mPZPIJAOBtsuOYSv04',
+//         secretKey: 'jrpVnTcK2I8xHqyCW8qCkTTHvXdw9THk6vGW',
+//         proxy: {
+//             host: "qna02.vitechcheap.com",
+//             port: 28052,
+//             auth: { username: "user_gnkogl", password: "hbwy0u7t" },
+//         }
+//     }
+// ]
 
 // const dataHuy2 = [
 //     { apiKey: 'oXdBcSLBLbSJWZ4o2T', secretKey: 'btakQB4KnZL8hPek73L1wD99ohskUVumCbRW' },
@@ -403,35 +403,67 @@ const dataHuy5 = [
 //     { apiKey: 'aCXpR1oaAZ1dCp3rW1', secretKey: 'pUKhZ5SDSVHkakd7PmVeyuMEHGT6hJLSY8XW' }
 // ];
 // const dataHuy1 = [
-//     { apiKey: 'whg9Lp6RWsaEllzBpZ', secretKey: 'QDBtKNoe55MAxtL0LLOmdu5am1ESkcdpn5b6' },
-//     { apiKey: 'SdCHr7s0r8mK8VQ6vV', secretKey: 'H8v8mbU8Ep3vwKAiF5FgLZLy9aHBUtWwa0FA' },
-//     { apiKey: 'MZID18SqCsvJTXFTD3', secretKey: 'KLPVhsddmkEuvE6KmBZDAuUBNLUuw2WdOYD2' },
-//     { apiKey: 'EyFkv4rDBdlYMiHbBV', secretKey: 'nrw5wcEVPc3dmUckYlcu8hBEf29Ah5DD5Nhs' },
-//     { apiKey: 'uhFED0bfcALgRcFtgf', secretKey: 'hWPy2A4E7r5RBWGwLovSdVShMFtHNxi0raZ8' },
-//     { apiKey: 'seTMBvPyFZdFAHSNts', secretKey: 'QZ89wRAw489AsvziIvbS2DabeIiF2qhs45fN' },
-//     { apiKey: 'xwsJ4HF7pbedcS4Q6R', secretKey: 'HlBcL1xyNdYxhFhGLJ6UXluvkpij1M4zcTFp' },
-//     { apiKey: 'pWDtoKBqmXPnYh4HRe', secretKey: 'b0wKmcc94a9fj9gwcnYMAL4leCgVHR8hkwiO' },
-//     { apiKey: '6hRjX1wghSoXsH9fKn', secretKey: '51g4pwjeD6YT8Ed6RCCmMg89E3epwGkr429b' },
-//     { apiKey: 'vzTqX5MhNxjQXPHWCK', secretKey: 'zvNjQ4cIMohrnRo2czqjcKpQYMvFrwnPsIin' },
+//     { apiKey: 'Cxcew2Xdx7rwqX0f3b', secretKey: 'BXPgPxOIMgF8rYDzyzfDLJawUdhB9MBKAlh3' },
+//     { apiKey: 'W32HHanwPIld4CLgBs', secretKey: 'Mg5HKMCtjjP89jz3xrFoKTKdCSjzp3oJVuA7' },
+//     { apiKey: 'fGwy5PZMWy6lcAqfra', secretKey: 'FuEx7NuI8GozpXFIBxbH4ROxDJW9fv43Cvej' },
+//     { apiKey: 'myD9ePm96ITbMTZQ8h', secretKey: 'Js6sxuchyhib7ZpsInUZfy6JhSUAUk8tvOI4' },
+//     { apiKey: '51ctcDxAxNx4sp6frU', secretKey: 'Pd7aVPDILa7WHJA3Wp9mq6znallIHlFRI4eU' },
+//     { apiKey: 'HCSPRjfGZ4sidHXWMO', secretKey: 'WdLBeeh5jypbHmP1vZFhippKJCRpZHSew8KA' },
+//     { apiKey: 'ETcf3IvuxvMlTPcGhv', secretKey: 'goQGS1X2M4g0Z06ORgXhaod1WiqjvrVSOI9H' },
+//     { apiKey: 'Tft3kEKBZ1POFs3LrQ', secretKey: 'MyMVr2osrwGsCOmBRiw1AeqGMlXeu0yv7frn' },
+//     { apiKey: '4cdwoLpPvcqZREjgwC', secretKey: 'GpOKPgZ8xAs6M1JkjmdPC3MMusJTmaimrQcg' },
+//     { apiKey: 'M8jW51YdEIu25WUfC4', secretKey: 'YtLHcKZxxM3vZWK6H9MaEiQWdAQ8JlnDMn4P' }
 // ];
 
 // const dataHuy2 = [
-//     { apiKey: '6UkT2PaKJtbsba2BiB', secretKey: 'g5Eenaw3br41sFZ6t4tNTeht02slXqZpt3eg' },
-//     { apiKey: 'bCtzcQwDwxK5XDSvWN', secretKey: '5NwvV6aU3wgglQ9uJWp8C473M7ncGqyHkDce' },
-//     { apiKey: 'ASDXOp4TT8p7HwD5Kk', secretKey: 'zpguOGNC8sE23oKZHh2YMvSSuA84mEA8uWdb' },
-//     { apiKey: 'b8PDTwkByQvuULtbCz', secretKey: '5itzUOergKoQRbGWAWeF9XrxEwPtZpVlF3qK' },
-//     { apiKey: 'A5mCHigKIVfQQ6IcTh', secretKey: 'ue4kJP9INHh3O9oOSKzOCZG4FWjMKIktbCfy' },
-//     { apiKey: 'zBsXqax2KgyQLFssQF', secretKey: 'zyWqRdUveUk6tvcbrNIiCS39V3Ojkh69SsOp' },
-//     { apiKey: 'uesUi3Eu9nhhK6TkJE', secretKey: 'elaQO0BeN2DXkdLzaAvg1MtF6Pz6PipAdAbk' },
-//     { apiKey: 'YOSnzlV8vmNk1lTasr', secretKey: 's8F0aaQOrLcAPnd5c0zS67yqVx3O060I27as' },
-//     { apiKey: '5S4lmd2Bv0MbBPAAGK', secretKey: '4hk8PmG7JcYk98uEhLFx4EWEBBkH8xtbeniC' },
-//     { apiKey: 'XH4JpFK5M6Ai7tuEGB', secretKey: 'Ll6O8KnTdibnbMKILb0OXPPSs1mHLbGaoB81' }
+//     { apiKey: 'oXdBcSLBLbSJWZ4o2T', secretKey: 'btakQB4KnZL8hPek73L1wD99ohskUVumCbRW' },
+//     { apiKey: 'xvI7ZIAFbMZiZjjgG7', secretKey: 'yApuT9dOnx7e2bj1XusoWrkHtJ2e7r2sGPLU' },
+//     { apiKey: 'CZ40IJtKepv5waPelu', secretKey: '58zIHVMsubXPrZZxJDgw0dCASRwd9ZxuHyTQ' },
+//     { apiKey: 'ub2KFS2CEgqQkzwAXv', secretKey: '16TkpUInDqXiRqNlPF1sodUhiXuIqHd6NkQ4' },
+//     { apiKey: 'GfmOZtY8tTCf36nmC6', secretKey: 'pygenAHtTQcK0Elgu04nEB75oMfvsRPMJy1H' },
+//     { apiKey: 'KBRRZ4i4e6mXkvQBze', secretKey: 'trXOtzGDZZ5JCf7kZN08GZxDxujhBLUa7MDk' },
+//     { apiKey: 'wocJSGg2BDigsShlhA', secretKey: 'jYzH4kiwu9at2IKaTeLOVuEphSID3DNG8U8R' },
+//     { apiKey: 'RADbCjkxxhqvMtK4Ud', secretKey: '9f1VSmbByTATDXFoAbR4lPRSvBuTYhMnzDqU' },
+//     { apiKey: 'DjWUEJii55b3rDF9EV', secretKey: 'TEgYJLtmfkPVh91HmK3QmYpEaguuOHYlrnvD' },
+//     { apiKey: 'mPZPIJAOBtsuOYSv04', secretKey: 'jrpVnTcK2I8xHqyCW8qCkTTHvXdw9THk6vGW' }
 // ];
 
-// const dataHuy3 = [
-//     { apiKey: 'SaZ7cqT05eCMTabNS5', secretKey: 'y80CDn5E04rDIHUU106vfP7y6TncQFXjAhJT' },
-//     { apiKey: 'Cxcew2Xdx7rwqX0f3b', secretKey: 'BXPgPxOIMgF8rYDzyzfDLJawUdhB9MBKAlh3' },
-//     { apiKey: 'W32HHanwPIld4CLgBs', secretKey: 'Mg5HKMCtjjP89jz3xrFoKTKdCSjzp3oJVuA7' },
-//     // { apiKey: 'PxGgLI7iWsQMeJ9Raq', secretKey: 'tFWc1FrVLrCWfWfJyue3DCeLzlctvGoS9xdf' },
+const dataHuy1 = [
+    { apiKey: 'Cxcew2Xdx7rwqX0f3b', secretKey: 'BXPgPxOIMgF8rYDzyzfDLJawUdhB9MBKAlh3' },
+    { apiKey: 'W32HHanwPIld4CLgBs', secretKey: 'Mg5HKMCtjjP89jz3xrFoKTKdCSjzp3oJVuA7' },
+    { apiKey: 'fGwy5PZMWy6lcAqfra', secretKey: 'FuEx7NuI8GozpXFIBxbH4ROxDJW9fv43Cvej' },
+    { apiKey: 'myD9ePm96ITbMTZQ8h', secretKey: 'Js6sxuchyhib7ZpsInUZfy6JhSUAUk8tvOI4' },
+    { apiKey: '51ctcDxAxNx4sp6frU', secretKey: 'Pd7aVPDILa7WHJA3Wp9mq6znallIHlFRI4eU' },
+]
+const dataHuy2 = [
+    { apiKey: 'HCSPRjfGZ4sidHXWMO', secretKey: 'WdLBeeh5jypbHmP1vZFhippKJCRpZHSew8KA' },
+    { apiKey: 'ETcf3IvuxvMlTPcGhv', secretKey: 'goQGS1X2M4g0Z06ORgXhaod1WiqjvrVSOI9H' },
+    { apiKey: 'Tft3kEKBZ1POFs3LrQ', secretKey: 'MyMVr2osrwGsCOmBRiw1AeqGMlXeu0yv7frn' },
+    { apiKey: '4cdwoLpPvcqZREjgwC', secretKey: 'GpOKPgZ8xAs6M1JkjmdPC3MMusJTmaimrQcg' },
+    { apiKey: 'M8jW51YdEIu25WUfC4', secretKey: 'YtLHcKZxxM3vZWK6H9MaEiQWdAQ8JlnDMn4P' }
+]
+const dataHuy3 = [
+    { apiKey: 'oXdBcSLBLbSJWZ4o2T', secretKey: 'btakQB4KnZL8hPek73L1wD99ohskUVumCbRW' },
+    { apiKey: 'xvI7ZIAFbMZiZjjgG7', secretKey: 'yApuT9dOnx7e2bj1XusoWrkHtJ2e7r2sGPLU' },
+    { apiKey: 'CZ40IJtKepv5waPelu', secretKey: '58zIHVMsubXPrZZxJDgw0dCASRwd9ZxuHyTQ' },
+    { apiKey: 'ub2KFS2CEgqQkzwAXv', secretKey: '16TkpUInDqXiRqNlPF1sodUhiXuIqHd6NkQ4' },
+    { apiKey: 'GfmOZtY8tTCf36nmC6', secretKey: 'pygenAHtTQcK0Elgu04nEB75oMfvsRPMJy1H' },
+]
+const dataHuy4 = [
+    { apiKey: 'KBRRZ4i4e6mXkvQBze', secretKey: 'trXOtzGDZZ5JCf7kZN08GZxDxujhBLUa7MDk' },
+    { apiKey: 'wocJSGg2BDigsShlhA', secretKey: 'jYzH4kiwu9at2IKaTeLOVuEphSID3DNG8U8R' },
+    { apiKey: 'RADbCjkxxhqvMtK4Ud', secretKey: '9f1VSmbByTATDXFoAbR4lPRSvBuTYhMnzDqU' },
+    { apiKey: 'DjWUEJii55b3rDF9EV', secretKey: 'TEgYJLtmfkPVh91HmK3QmYpEaguuOHYlrnvD' },
+    { apiKey: 'mPZPIJAOBtsuOYSv04', secretKey: 'jrpVnTcK2I8xHqyCW8qCkTTHvXdw9THk6vGW' }
+]
+// const dataHuy4 = [
+//     // { apiKey: 'Cxcew2Xdx7rwqX0f3b', secretKey: 'BXPgPxOIMgF8rYDzyzfDLJawUdhB9MBKAlh3' },
+//     // { apiKey: 'W32HHanwPIld4CLgBs', secretKey: 'Mg5HKMCtjjP89jz3xrFoKTKdCSjzp3oJVuA7' },
+//     // { apiKey: 'fGwy5PZMWy6lcAqfra', secretKey: 'FuEx7NuI8GozpXFIBxbH4ROxDJW9fv43Cvej' },
 // ];
+const dataHuy5 = [
+    // { apiKey: 'Cxcew2Xdx7rwqX0f3b', secretKey: 'BXPgPxOIMgF8rYDzyzfDLJawUdhB9MBKAlh3' },
+    // { apiKey: 'W32HHanwPIld4CLgBs', secretKey: 'Mg5HKMCtjjP89jz3xrFoKTKdCSjzp3oJVuA7' },
+    // { apiKey: 'fGwy5PZMWy6lcAqfra', secretKey: 'FuEx7NuI8GozpXFIBxbH4ROxDJW9fv43Cvej' },
+];
 module.exports = { dataHuy1, dataHuy2, dataHuy3, dataHuy4, dataHuy5 };
