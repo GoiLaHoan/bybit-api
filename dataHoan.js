d63f5fa304f069badce67f02a017c1b50e164cfb
GoiLaHoan <nvhoan911@gmail.com>
2024-03-20 18:47:29 +0700

    data Hoan1

--- dataHoan.js
+++ dataHoan.js
@@ -1,41 +1,23 @@
 const dataHoan1 = [
-  {
-    apiKey: "hzmhn4Er33lKYCJfMw",
-    secretKey: "BAPhAl4Je46Ea7cyJBaqrEOgYKS9vN2mVVMz",
-    proxy: {
-      host: "qna04.vitechcheap.com",
-      port: 31020,
-      auth: { username: "user_7x8dp", password: "frd5wax8" },
-    },
-  },
-  {
-    apiKey: "opT5zCcnwbon33zSNm",
-    secretKey: "zFw2tbVjvntC6jKiwqYipkGQmVpTMsgU2htX",
-    proxy: {
-      host: "qna04.vitechcheap.com",
-      port: 31003,
-      auth: { username: "user_7x8dp", password: "frd5wax8" },
-    },
-  },
-  {
-    apiKey: "VGUI5wtHepOaUZ1FHe",
-    secretKey: "OiTrWsPTp87bJdHNrDBVN0pXp1x3s6QhmCE8",
-    proxy: {
-      host: "qna04.vitechcheap.com",
-      port: 31037,
-      auth: { username: "user_7x8dp", password: "frd5wax8" },
-    },
-  },
-  {
-    apiKey: "euFktPMJGiQoAcxiu5",
-    secretKey: "y0Zz0sKTtUihIgC7XlkLkAmv91YqKaFEoUC7",
-    proxy: {
-      host: "qna04.vitechcheap.com",
-      port: 31079,
-      auth: { username: "user_7x8dp", password: "frd5wax8" },
-    },
-  },
+  { apiKey: "VGUI5wtHepOaUZ1FHe", secretKey: "OiTrWsPTp87bJdHNrDBVN0pXp1x3s6QhmCE8"},
+  { apiKey: "rKcfKb80DrCJCyHn91", secretKey: "iRiUplIOfs9evS25jpsoby1r8PHudS6Y9xJ3"},
+  { apiKey: "mU2U0jqFO77sEcT96y", secretKey: "nO7t8YFtyZ1zT2XnOi1izxCjQDMmEFyvbcuj"},
+  { apiKey: "GZBRPvCxWhC5KajPf5", secretKey: "Xw8FHgyVO8iLCS2azVQSAktNwUEc4mWxkkTL"},
+  { apiKey: "6Gkmwj74dMikLL8oR2", secretKey: "JIrPKii58gWYZy4PCEQr7wojtC6hlSq5xHx2"},
+  { apiKey: "jMTV3n1pWM8GXeKbV3", secretKey: "IOvLlKtXsRtn7tVSP6dNXvlHChLH4yiKwB9I"},
+  { apiKey: "NgVI9kt8KXUAiDyRd9", secretKey: "2jbb6SuQLBzYFTdrwKwwvn0jDnym2cAsyHWw"},
+  { apiKey: "IAIuRdoWenLsDbAkNw", secretKey: "HKTvtAhtj89gGC4anzl1gOmmp2V5cg6yyAGo"},
+  { apiKey: "n2Lf5V1GVKzZgWYeRs", secretKey: "c2KHxUJBM5v6mrb8vuFl0lPntBEwhx93hOi9"},
+  { apiKey: "LCAs9EdGjbKt0JsroJ", secretKey: "7mugtnxEwzRApU4bhkKg3V3aamIEMZdRxC2e"},
+  { apiKey: "GvpY3CUhks6djW2yGF", secretKey: "F5DBpULG7b5SDMmiatr21R63PUVjdlAhBsv6"},
+  { apiKey: "QNHBh4Siz74GhUPYoX", secretKey: "A0aAxUM0NQjqzHAQVXEznSt8h1xDJ3GJRi61"},
+  { apiKey: "0r3YvgLLAnvT3gC90u", secretKey: "uST6YnvlN2D7hRElRGHl1ZZZObwBncR6Oskk"},
+  { apiKey: "MVBNXslGkdVyQlUv2l", secretKey: "uxRvfEzmw43Gd4yUpz5jQoQnv2bRUTK5ZnD8"},
+  { apiKey: "7hmK8J7Ud2UpjIfYPR", secretKey: "qVjfLi4X3fqS6BqliLz3KsQvbEO5PSJYK1jw"},
+  { apiKey: "FAWphhKuZ8riUk3X4c", secretKey: "bcVcCBtxUVF7PyGjaEA7YG7Sw3idSaPJayT3"},
+  { apiKey: "heHq7EhniSA4xCVnaV", secretKey: "X5SCqfofrsZnOQXx8jXcvXSEO98F8GSrC3pZ"},
 ];
+
 const dataHoan2 = [
   {
     apiKey: "8IbHSfXhFHyCg3GEaI",
